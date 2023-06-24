8eefc82865cb232a49f86d9dcde00fed4d958712
Manura <w.m.manurasanjula2003@outlook.com>
2023-06-24 23:47:45 +0530

    Add In-Memory DataBase

---
+++ BixbyShop-LK/BixbyShop-LK/Config/MyDbContext.cs
@@ -0,0 +1,34 @@
+﻿using BixbyShop_LK.Models.Comments;
+using BixbyShop_LK.Models.Item;
+using BixbyShop_LK.Models.Order;
+using BixbyShop_LK.Users_and_Roles;
+using Microsoft.EntityFrameworkCore;
+
+namespace BixbyShop_LK.Config
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Authority> Authorities { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<ShopItem> ShopItems { get; set; }
+        public DbSet<CartAndOrder> CartAndOrders { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("Bixby_ShopAPP_InMemoryDb");
+        }
+    }
+}
